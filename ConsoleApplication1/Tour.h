b677b091b66409923929eece0f592b222f51a5db
J-F <jffontaine_@hotmail.com>
2017-02-10 10:12:13 -0500

    Implementation Tour

---
+++ ConsoleApplication1/Tour.h
@@ -0,0 +1,32 @@
+/**********************************************
+* Titre: Travail pratique #1 - Tour.h
+* Date: 10 janvier 2017
+* Auteur: FERRON Samuel, FONTAINE Jean-Frederic
+*Description: Définition de la classe Tour
+*********************************************/
+#pragma once
+#include<Piece.h>
+#include<string>
+using namespace std; 
+class Tour : public Piece{
+
+public:
+	//Constructeur par defaut 
+	Tour(); 
+	//Constructeur par parametre
+	Tour(string id, string couleur, int positionX, int positionY);
+	//Destructeur 
+	~Tour(); 
+
+	//Fonction verifiant la validite d'un mouvement 
+	bool estMouvementValide(int toX, int toY);
+	//Fontion pour effectuer un deplacement 
+	void deplacer(int toX, int toY);
+
+
+
+private:
+
+
+
+}
